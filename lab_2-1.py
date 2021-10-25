33008e00c2db27948f73fdc0a9850c335119f063
Jordan Hart <p22jhart@fairfieldprep.org>
2021-10-25 06:53:03 -0700

    c2 10/25/21

---
+++ lab_2-1.py
@@ -0,0 +1,16 @@
+
+import math
+
+math.ceil(7.15)
+print(math.ceil(7.15))
+
+math.floor(19.156)
+print(math.floor(19.156))
+math.fabs(-42)
+print(math.fabs(-42))
+
+math.pow(2, 3)
+print(math.pow(2, 3))
+
+math.log10(100)
+print(math.log10(100))
