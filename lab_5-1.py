831fdbef59f2e9d5c602feae51cc36425d2f432a
Jordan Hart <p22jhart@fairfieldprep.org>
2021-10-27 07:18:13 -0700

    c5 10/27/21

---
+++ lab_5-1.py
@@ -0,0 +1,9 @@
+school="fairfield Prep"
+
+first_half= school[:9]
+
+second_half=school[9:]
+
+print(first_half + second_half)
+
+
---
+++ lab_5-2.py
@@ -0,0 +1,11 @@
+
+word1 =input("give me a word:")
+
+word2= input("give me another word:")
+
+if word1 < word2:
+    print(word1 + " comes before " + word2)
+elif  word1 > word2:
+    print(word1 +"comes after" + word2)
+else:
+    print(word1 + "and")
